2649eaa4be144068ad1c82a7b297932ba26c5624
Inder24 <likhariinder@gmail.com>
2021-08-26 13:07:42 +0530

    Added main files for Cognito Integration with JS

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_M70WV8Saap',
+        region: 'ap-southeast-1',
+        clientId: '34qjf1nvh93gjea847sd4iul5v2abcd'
+    },
+};
